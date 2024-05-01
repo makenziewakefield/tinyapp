fc207cf59d3bb31355f10d2de0ab4487c71f18d1
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-30 21:14:07 -0600

    Extract username from request body

--- express_server.js
+++ express_server.js
@@ -89,6 +89,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // POST route to handle requests to login
 app.post("/login", (req, res) => {
+  const username = req.body.user;
   res.cookie('username', username);
   res.redirect("/urls");
 })

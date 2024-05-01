78719bbdd7d5c11f26e02a97da87fbeffcd5fd91
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-30 21:11:36 -0600

    Used res.cookie() to set a cookie

--- express_server.js
+++ express_server.js
@@ -89,7 +89,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // POST route to handle requests to login
 app.post("/login", (req, res) => {
-  const { username } = req.body;
+  res.cookie('username', username);
   res.redirect("/urls");
 })
 

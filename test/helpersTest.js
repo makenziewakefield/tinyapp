9e5e7b7bda797054de6cbbd5f24f9dcd23c526f6
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-05-11 13:58:15 -0600

    Added test cases

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,11 @@
 const { assert } = require('chai');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
 
 const { getUserByEmail } = require('../helpers.js');
+
+chai.use(chaiHttp);
 
 const testUsers = {
   "userRandomID": {
@@ -33,4 +38,55 @@ describe('getUserByEmail', function() {
     assert.isUndefined(userNull, 'getUserByEmail should return undefined for null email');
     assert.isUndefined(userUndefined, 'getUserByEmail should return undefined for undefined email');
   });
+});
+
+
+describe("Login and Access Control Test", () => {
+  const agent = chai.request.agent("http://localhost:8080");
+
+  it('should return 403 status code for unauthorized access to "http://localhost:8080/urls/b2xVn2"', () => {
+    // Step 1: Login with valid credentials
+    return agent
+      .post("/login")
+      .send({ email: "user2@example.com", password: "dishwasher-funk" })
+      .then((loginRes) => {
+        // Step 2: Make a GET request to a protected resource
+        return agent.get("/urls/b2xVn2").then((accessRes) => {
+          // Step 3: Expect the status code to be 403
+          expect(accessRes).to.have.status(403);
+        });
+      });
+  });
+
+
+it('should return 403 for authenticated user without access to the URL', () => {
+  return agent
+    .get('/urls/:id')
+    .then((res) => {
+      expect(res).to.have.status(403);
+    });
+});
+
+
+  it('should return 404 for GET request to a non-existing URL', () => {
+    return agent
+      .get('/urls/NOTEXISTS')
+      .then((res) => {
+        expect(res).to.have.status(403);
+      });
+  });
+
+  it('should return 403 for GET request to "/urls/b2xVn2"', () => {
+    return agent
+      .get('/urls/b2xVn2')
+      .then((res) => {
+        expect(res).to.have.status(403);
+      });
+  });
+
+  after(() => {
+    // Close agent after tests
+    agent.close();
+  });
+
 });

10aff9b824175176f8d35a2f965d9e038f71521f
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-05-11 18:49:37 -0600

    Cleaning up code

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -45,14 +45,11 @@ describe("Login and Access Control Test", () => {
   const agent = chai.request.agent("http://localhost:8080");
 
   it('should return 403 status code for unauthorized access to "http://localhost:8080/urls/b2xVn2"', () => {
-    // Step 1: Login with valid credentials
     return agent
       .post("/login")
       .send({ email: "user2@example.com", password: "dishwasher-funk" })
       .then((loginRes) => {
-        // Step 2: Make a GET request to a protected resource
         return agent.get("/urls/b2xVn2").then((accessRes) => {
-          // Step 3: Expect the status code to be 403
           expect(accessRes).to.have.status(403);
         });
       });
